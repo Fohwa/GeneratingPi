8b31dfc1d87f70c843abdc857f2d718157dead0f
Fohwa <78933542+Fohwa@users.noreply.github.com>
2021-05-16 22:36:32 +0200

    first commit

---
+++ pi.py
@@ -0,0 +1,31 @@
+import random
+
+times = int(input("Number of points: "))
+
+incircle = 0
+insquare = 0
+
+while times >= 0:
+    
+    ran1 = random.uniform(0, 1)
+    ran2 = random.uniform(0, 1)
+
+    if ran1**2 + ran2**2 <= 1: #in circle
+        incircle += 1
+        insquare += 1
+    elif ran1**2 + ran2**2 > 1: #not in circle
+        insquare += 1
+    else:
+        print("Error: Point not in circle and not outside")
+    
+    
+    times = times - 1
+
+pi = 4 * incircle/insquare # see joma video for confirmation: Ration of points times four is indeed pi
+
+#just for some additional information
+print("In: " + str(incircle))
+print("Outside: " + str(insquare - incircle))
+
+#The result is printed to console
+print("Pi: " + str(pi))
